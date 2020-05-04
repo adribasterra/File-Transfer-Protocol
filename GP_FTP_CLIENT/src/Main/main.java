9a6dedebce347ee930b50196f941f180b2cdbad7
HirwaShemaLin <linhirwa@gmail.com>
2020-05-04 18:28:29 +0200

    Modification of main server and client to text

--- GP_FTP_CLIENT/src/Main/main.java
+++ GP_FTP_CLIENT/src/Main/main.java
@@ -1,14 +1,14 @@
 package Main;
 
 import FTP_Client.DataClient;
-//import FTP_Client.TestClient;
+import FTP_Client.TextClient;
 
 public class main
 {
 	public static void main(String[] args)
 	{
 		System.out.println("CLIENT\n\n");
-		//TestClient.testClient();
-		DataClient.testClient();
+		TextClient.testClient();
+		//Client.testClient();
 	}
 }
--- GP_FTP_SERVER/src/Main/main.java
+++ GP_FTP_SERVER/src/Main/main.java
@@ -1,6 +1,6 @@
 package Main;
 
-//import FTP_Server.TextServer;
+import FTP_Server.TextServer;
 import FTP_Server.DataServer;
 
 public class main
@@ -8,7 +8,7 @@ public class main
 	public static void main(String[] args)
 	{
 		System.out.println("SERVER\n\n");
-		//TextServer.testServer();
-		DataServer.testServer();
+		TextServer.testServer();
+		//DataServer.testServer();
 	}
 }
